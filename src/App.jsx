ab9eb7760fde0a439028a47aaaf42199bda9398a
Ngpaterson <patersonnguepi@gmail.com>
2023-05-22 14:28:37 +0200

    Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './index.css';
 import Header from './components/header/Header'
 import About from './components/about/About'
 import Navbar from './components/navbar/Navbar';
+import Contact from './components/contact/Contact';
 function App() {
 return (
 <>
 <Header/>
 <Navbar/>
 <About/>
+<Contact/>
 </>
 );
 }
