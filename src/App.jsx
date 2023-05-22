d3f1bb3e512b4b0c55e505ddd7f2cc8364343978
Ngpaterson <patersonnguepi@gmail.com>
2023-05-22 06:10:08 +0200

    Navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './index.css';
 import Header from './components/header/Header'
 import About from './components/about/About'
+import Navbar from './components/navbar/Navbar';
 function App() {
 return (
 <>
 <Header/>
+<Navbar/>
 <About/>
 </>
 );
