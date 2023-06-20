0a3dc0d7ce5cba44aa9c0cb8e44936da1bdfa343
Ngpaterson <patersonnguepi@gmail.com>
2023-06-21 00:28:00 +0200

    Data_update

--- model/portfolio_model.js
+++ model/portfolio_model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const portfoliomodel = new mongoose.model("projects", mongoose.Schema({
+const portfoliomodel = new mongoose.model("portfolio", mongoose.Schema({
 
     _id: String,
     image: String,
--- playground-3.mongodb.js
+++ playground-3.mongodb.js
@@ -2,52 +2,52 @@
 use('paterson');
 
 // Insert a few documents into the PortfolioData collection.
-db.getCollection('projects').insertMany(
-    [
-        {
+// db.getCollection('projects').insertMany(
+//     [
+//         {
           
-            image: 'https://cdn.dribbble.com/userupload/7518943/file/original-b39a16475b25ac10559e04034bd8b9b7.png?compress=1&resize=1504x1128',
-            title: 'Online Crypto Casino',
-            github: 'https://github.com',
-            demo: 'https://dribbble.com/shots/21632973-Online-Crypto-Casino-Mobile-Designs',
-        },
-        {
+//             image: 'https://cdn.dribbble.com/userupload/7518943/file/original-b39a16475b25ac10559e04034bd8b9b7.png?compress=1&resize=1504x1128',
+//             title: 'Online Crypto Casino',
+//             github: 'https://github.com',
+//             demo: 'https://dribbble.com/shots/21632973-Online-Crypto-Casino-Mobile-Designs',
+//         },
+//         {
        
-            image: 'https://cdn.dribbble.com/userupload/7531024/file/original-28960363b0b1e5a03c2f6f1f077c298e.jpg?compress=1&resize=1504x1128',
-            title: 'Education App UIX',
-            github: 'https://github.com',
-            demo: 'https://dribbble.com/shots/21637001-Education-App-UIX',
-        },
-        {
-            image: 'https://cdn.dribbble.com/userupload/7474794/file/original-ec5e3ff0615f4ec018a8662d76206bde.jpg?compress=1&resize=1504x1126',
-            title: 'Recipe app design',
-            github: 'https://github.com',
-            demo: 'https://dribbble.com/shots/21617723-Recipe-app-design',
-        },
-        {
+//             image: 'https://cdn.dribbble.com/userupload/7531024/file/original-28960363b0b1e5a03c2f6f1f077c298e.jpg?compress=1&resize=1504x1128',
+//             title: 'Education App UIX',
+//             github: 'https://github.com',
+//             demo: 'https://dribbble.com/shots/21637001-Education-App-UIX',
+//         },
+//         {
+//             image: 'https://cdn.dribbble.com/userupload/7474794/file/original-ec5e3ff0615f4ec018a8662d76206bde.jpg?compress=1&resize=1504x1126',
+//             title: 'Recipe app design',
+//             github: 'https://github.com',
+//             demo: 'https://dribbble.com/shots/21617723-Recipe-app-design',
+//         },
+//         {
         
-            image: 'https://cdn.dribbble.com/userupload/7527073/file/original-a56488a4a04a23bfbeb97cafc030dde5.png?compress=1&resize=1504x1128',
-            title: 'OnStep - Shoes Store',
-            github: 'https://github.com',
-            demo: 'https://dribbble.com/shots/21635811-OnStep-Shoes-Store',
-        },
-        {
+//             image: 'https://cdn.dribbble.com/userupload/7527073/file/original-a56488a4a04a23bfbeb97cafc030dde5.png?compress=1&resize=1504x1128',
+//             title: 'OnStep - Shoes Store',
+//             github: 'https://github.com',
+//             demo: 'https://dribbble.com/shots/21635811-OnStep-Shoes-Store',
+//         },
+//         {
      
-            image: 'https://cdn.dribbble.com/users/5147614/screenshots/17480773/media/77bbee6dafd811f65d14bd38f4c426ab.png?compress=1&resize=1200x900&vertical=top',
-            title: 'Mobile App Exploration',
-            github: 'https://github.com',
-            demo: 'https://dribbble.com/shots/17480773-NWS-Mobile-App-Exploration',
-        },
-        {
+//             image: 'https://cdn.dribbble.com/users/5147614/screenshots/17480773/media/77bbee6dafd811f65d14bd38f4c426ab.png?compress=1&resize=1200x900&vertical=top',
+//             title: 'Mobile App Exploration',
+//             github: 'https://github.com',
+//             demo: 'https://dribbble.com/shots/17480773-NWS-Mobile-App-Exploration',
+//         },
+//         {
        
-            image: 'https://cdn.dribbble.com/userupload/7528445/file/original-4b13d4c68619e9e932565427083bb19d.jpg?compress=1&resize=1504x1126',
-            title: 'Recipe app',
-            github: 'https://github.com',
-            demo: 'https://dribbble.com/shots/21636337-Recipe-app',
-        }
+//             image: 'https://cdn.dribbble.com/userupload/7528445/file/original-4b13d4c68619e9e932565427083bb19d.jpg?compress=1&resize=1504x1126',
+//             title: 'Recipe app',
+//             github: 'https://github.com',
+//             demo: 'https://dribbble.com/shots/21636337-Recipe-app',
+//         }
 
-    ]
-); 
+//     ]
+// ); 
 
 // Insert a few documents into the PortfolioData collection.
 // db.getCollection('testimonials').insertMany(
@@ -77,3 +77,16 @@ db.getCollection('projects').insertMany(
       
 //       ]
 // );
+
+
+db.getCollection('portfolio').insertMany(
+    [
+           {
+             image: 'https://ibb.co/7GY9jjs',
+             title: 'Portfolio',
+             github: 'https://github.com/Ngpaterson/CEF344-PROJECT.git',
+             demo: 'https://paterson-nguepi.netlify.app',
+           }
+   
+            ]
+   );
